6e2ba1dd79c4ff4305feb2b705b6647c48159093
ashima-77 <ashimaarora2005@gmail.com>
2026-04-05 22:39:06 +0530

    fix image upload path for vercel

--- NodeJSMVC/controllers/CustomerController.js
+++ NodeJSMVC/controllers/CustomerController.js
@@ -51,7 +51,8 @@ console.log("**********");
     if(req.files&&req.files.profilepic)
     {
      fileName=req.files.profilepic.name;
-    let uploadsFolderPath=path.join(__dirname,"..","uploads",fileName);
+    // let uploadsFolderPath=path.join(__dirname,"..","uploads",fileName);
+    let uploadsFolderPath="/tmp/"+fileName;
      await req.files.profilepic.mv(uploadsFolderPath);
     //****************Sending to cloudinary server******************* */
         await cloudinary.uploader.upload(uploadsFolderPath).then(function(picUrlResult){
